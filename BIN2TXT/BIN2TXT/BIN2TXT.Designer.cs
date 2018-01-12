500b2d604f74aad10f2445b772a83dbf8e60cba0
fanchseeyou <2277890246@qq.com>
2018-01-12 10:38:39 +0800

    第一次提交git

---
+++ BIN2TXT/BIN2TXT/BIN2TXT.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace BIN2TXT
+{
+    partial class BIN2TXT
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BIN2TXT));
+            this.labelRead = new System.Windows.Forms.Label();
+            this.textBoxRead = new System.Windows.Forms.TextBox();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.labelWrite = new System.Windows.Forms.Label();
+            this.textBoxWrite = new System.Windows.Forms.TextBox();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.richTextBoxData = new System.Windows.Forms.RichTextBox();
+            this.groupBoxData = new System.Windows.Forms.GroupBox();
+            this.groupBoxData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelRead
+            // 
+            this.labelRead.AutoSize = true;
+            this.labelRead.Location = new System.Drawing.Point(12, 23);
+            this.labelRead.Name = "labelRead";
+            this.labelRead.Size = new System.Drawing.Size(71, 12);
+            this.labelRead.TabIndex = 0;
+            this.labelRead.Text = "BIN文件名：";
+            // 
+            // textBoxRead
+            // 
+            this.textBoxRead.Location = new System.Drawing.Point(89, 20);
+            this.textBoxRead.Name = "textBoxRead";
+            this.textBoxRead.ReadOnly = true;
+            this.textBoxRead.Size = new System.Drawing.Size(138, 21);
+            this.textBoxRead.TabIndex = 1;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(233, 18);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(39, 23);
+            this.buttonRead.TabIndex = 2;
+            this.buttonRead.Text = "读取";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // labelWrite
+            // 
+            this.labelWrite.AutoSize = true;
+            this.labelWrite.Location = new System.Drawing.Point(12, 55);
+            this.labelWrite.Name = "labelWrite";
+            this.labelWrite.Size = new System.Drawing.Size(71, 12);
+            this.labelWrite.TabIndex = 3;
+            this.labelWrite.Text = "TXT文件名：";
+            // 
+            // textBoxWrite
+            // 
+            this.textBoxWrite.Location = new System.Drawing.Point(89, 52);
+            this.textBoxWrite.Name = "textBoxWrite";
+            this.textBoxWrite.ReadOnly = true;
+            this.textBoxWrite.Size = new System.Drawing.Size(138, 21);
+            this.textBoxWrite.TabIndex = 4;
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Location = new System.Drawing.Point(233, 50);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(39, 23);
+            this.buttonWrite.TabIndex = 5;
+            this.buttonWrite.Text = "写入";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // richTextBoxData
+            // 
+            this.richTextBoxData.Location = new System.Drawing.Point(6, 20);
+            this.richTextBoxData.Name = "richTextBoxData";
+            this.richTextBoxData.ReadOnly = true;
+            this.richTextBoxData.Size = new System.Drawing.Size(281, 149);
+            this.richTextBoxData.TabIndex = 6;
+            this.richTextBoxData.Text = "";
+            // 
+            // groupBoxData
+            // 
+            this.groupBoxData.Controls.Add(this.richTextBoxData);
+            this.groupBoxData.Location = new System.Drawing.Point(0, 84);
+            this.groupBoxData.Name = "groupBoxData";
+            this.groupBoxData.Size = new System.Drawing.Size(287, 175);
+            this.groupBoxData.TabIndex = 7;
+            this.groupBoxData.TabStop = false;
+            this.groupBoxData.Text = "数据区（只预览前2048个字节）";
+            // 
+            // BIN2TXT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 262);
+            this.Controls.Add(this.groupBoxData);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.textBoxWrite);
+            this.Controls.Add(this.labelWrite);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.textBoxRead);
+            this.Controls.Add(this.labelRead);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(307, 300);
+            this.MinimumSize = new System.Drawing.Size(307, 300);
+            this.Name = "BIN2TXT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BIN2TXT【QQ:728866519】";
+            this.groupBoxData.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelRead;
+        private System.Windows.Forms.TextBox textBoxRead;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Label labelWrite;
+        private System.Windows.Forms.TextBox textBoxWrite;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.RichTextBox richTextBoxData;
+        private System.Windows.Forms.GroupBox groupBoxData;
+    }
+}
+
---
+++ BIN2TXT/BIN2TXT/BIN2TXT.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BIN2TXT
+{
+    public partial class BIN2TXT : Form
+    {
+        string File_read_path = "";
+        string File_write_path = "";
+        public BIN2TXT()
+        {
+            InitializeComponent();
+        }
+
+        private string getFontName(string path)
+        {
+            string a = "";
+            string[] strArray = path.Split('\\');
+            a = strArray[strArray.Length - 1];
+            return a;
+        }
+
+        private void readBINData() {
+            try
+            {
+                string str = "";
+                UInt32 readLen = 0;
+                int i;
+                FileInfo ENFileInfo = new FileInfo(File_read_path);
+                readLen = (UInt32)ENFileInfo.Length;
+                if (readLen > 2048)
+                {
+                    readLen = 2048;
+                }
+                FileStream f_read = new FileStream(File_read_path, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(f_read);
+                br.BaseStream.Seek(0, SeekOrigin.Begin);
+                for (i = 0; i < readLen; i++)
+                {
+                    str += br.ReadByte().ToString("X2").ToUpper()+" ";
+                }
+                richTextBoxData.Text = str;
+            }
+            catch (Exception m)
+            {
+                MessageBox.Show(this, m.Message, "小凡", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog rFile = new OpenFileDialog();
+                rFile.Filter = "二进制文件|*.bin|LCD文件|*.LCD";//!<过滤文件类型
+                rFile.RestoreDirectory = true; //!<记忆上次浏览路径
+                if (rFile.ShowDialog() == DialogResult.OK)
+                {
+                    File_read_path = rFile.FileName;
+                    readBINData();
+                    textBoxRead.Text = getFontName(rFile.FileName);
+                }
+                rFile.Dispose();
+            }
+            catch (Exception m)
+            {
+                MessageBox.Show(this, m.Message, "小凡", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            int i;
+            UInt32 readLen = 0;
+            string str = "";
+            try
+            {
+                //设置保存文件的格式
+                SaveFileDialog mySave = new SaveFileDialog();
+                mySave.Filter = "文本文件(*.txt)|*.txt";
+                if (mySave.ShowDialog() == DialogResult.OK)
+                {
+                    //使用“另存为”对话框中输入的文件名实例化StreamWriter对象
+                    StreamWriter sw = new StreamWriter(mySave.FileName, false);
+                    FileInfo FileInfo = new FileInfo(File_read_path);
+                    readLen = (UInt32)FileInfo.Length;
+                    FileStream f_read = new FileStream(File_read_path, FileMode.Open, FileAccess.Read);
+                    BinaryReader br = new BinaryReader(f_read);
+                    br.BaseStream.Seek(0, SeekOrigin.Begin);
+                    for (i = 0; i < readLen; i++)
+                    {
+                        str = br.ReadByte().ToString("X2").ToUpper() + " ";
+                        sw.Write(str);
+                    }
+                    sw.Close();
+                }
+                textBoxWrite.Text = getFontName(mySave.FileName);
+                MessageBox.Show(this, "保存为" + getFontName(mySave.FileName) + "成功！", "小凡提示：", MessageBoxButtons.OK, MessageBoxIcon.None);
+            }
+            catch (Exception m)
+            {
+                MessageBox.Show(this, m.Message, "小凡", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
